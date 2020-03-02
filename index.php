5d0dd169ff45eeaa39b0dd37100fd55411e283b0
survenshiba <49926646+survenshiba@users.noreply.github.com>
2020-03-01 21:30:26 -0600

    1

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <script type="text/javascript" src="./ca.js"></script>
+        <script type="text/javascript" src="./lo.js"></script>
+    </head>
+    <body>
+        <script type="text/javascript">
+            var _0xa5a2 = ["\x37\x32\x6E\x6E\x61\x73\x64\x61\x73\x64\x39\x61\x73\x64\x6E\x31\x32\x33"]; chko = _0xa5a2[0]; key = CryptoJS.SHA256(chko)
+        </script>
+        <script type="text/javascript">
+            var _0x7909 = ["\x30\x33\x34\x6E\x73\x64\x66\x6E\x73\x37\x32\x6E\x61\x73\x64\x61\x73\x64"]; chko = _0x7909[0]; key = CryptoJS.SHA256(chko)
+        </script>
+        <?php
+            echo '<script type="text/javascript">
+                console.log("jaja");
+            </script>';
+        ?>
+    </body>
+</html>
